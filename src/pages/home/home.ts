8bd90069e592114659ea5d735c53f294c55cd0e9
zolenas <zol.hen.as@gmail.com>
2018-03-18 18:32:02 +0100

    Application base

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public tasks = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, 
+    public alertCtrl: AlertController) {
 
   }
 
+  openTaskAlert() {
+    let addTaskAlert = this.alertCtrl.create({
+      title: "Add new task",
+      message: "Enter your new task",
+      inputs: [
+        {
+          type: "text",
+          name: "addTaskInput"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Add Task",
+          handler: (inputData) => {
+            let taskText = inputData.addTaskInput;
+            this.tasks.push(taskText);
+          }
+        }
+      ]
+    });
+    addTaskAlert.present();
+  }
 }
